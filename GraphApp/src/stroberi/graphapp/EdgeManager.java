23f9ba9a978729298b299c09ac7938a79d377f87
GitStroberi <andreipilif@gmail.com>
2023-10-10 10:24:12 +0300

    Fixed not saving to matrix and panel not refreshing

--- GraphApp/src/stroberi/graphapp/EdgeManager.java
+++ GraphApp/src/stroberi/graphapp/EdgeManager.java
@@ -113,5 +113,7 @@ public class EdgeManager {
         for (Edge edge : newEdges) {
             graphPanel.addEdge(edge);
         }
+        graphPanel.getAdjacencyMatrix().saveMatrixToFile();
+        graphPanel.repaint();
     }
 }
